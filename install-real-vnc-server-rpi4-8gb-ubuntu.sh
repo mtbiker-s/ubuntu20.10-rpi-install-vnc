43538cebfb43bc2d205a5d7385d661fcd3a2695c
carlo <carloesanchez@gmail.com>
2021-08-22 10:59:27 -0700

    Adding more logic for the script to only modify the /etc/gdm3/custom.conf file when version is not Groovy Gorilla 20.10

--- install-real-vnc-server-rpi4-8gb-ubuntu.sh
+++ install-real-vnc-server-rpi4-8gb-ubuntu.sh
@@ -12,10 +12,13 @@ vncServer="realvnc-vnc-server_6.7.2.43081_arm64.deb"
 
 releaseInfo=`lsb_release -a`
 
+
+
 # Array of Ubuntu versions that need to have the
 # file custom.conf under /etc/gdm3 modified
+# 20.10 = Groovy Gorilla
 # 21.04 = Hirsute Hippo
-declare -a lookForVersions=( "21.04" )
+declare -a lookForVersions=( "20.10" "21.04" )
 
 # This function should only be called for the versions
 # of Ubuntu RPi like Hirsute Hippo v21.04 so that the 
@@ -28,6 +31,12 @@ function modifyGDM3CustomConf(){
     # Check to see if the version is Hirsute Hippo 21.04
     # Make backup of custom.conf under /etc/gdm3
     # replace the #WaylandEnabled=false with out it being commented out
+    gdm3File="/etc/gdm3/custom.conf"
+    echo "Attempting to make a backup of $gdm3File..."
+    timeStamp=$(date '+%Y%m%d-%H%M%S')
+    sudo cp -rfv "$gdm3File $gdm3File.$timeStamp" 
+    #echo $timeStamp
+    echo "test"
 
 }
 
@@ -37,11 +46,22 @@ function main(){
 
     echo "You are about to install RealVNC Server for RaspberryPi4 8gb"
     echo $vncServer
-    echo "Running OS:"
-    echo $releaseInfo
-    exit
     echo
-    echo
+    echo "Checking to see if this version of Ubuntu for RaspberryPi is supported by script..."
+
+    for version in "${lookForVersions[@]}"
+    do
+        if [[ "$releaseInfo" == *"$version"* ]]; then
+            echo "$releaseInfo"
+            echo "is supported by this script."
+        else
+            echo "$releaseInfo"
+            echo "Is not supported, exiting program..."
+            exit
+
+        fi
+    done
+
 
     echo "Would you like to continue?"
     echo "Enter 'y' for yes or 'n' for No :"
@@ -49,7 +69,7 @@ function main(){
 
     if [[ $answer == 'y' || $answer == 'Y' ]]; then
 
-
+        exit
         # Download package required
         echo
         echo "Installing VNC Server : \"$vncServer\"..."
@@ -101,7 +121,9 @@ function main(){
         # modifyGDM3CustomConf
         for version in "${lookForVersions[@]}"
         do
-            if [[ "$releaseInfo" == *"$version"* ]]; then
+            if [[ "$version" != "20.10" ]]; then
+                # Call function to do the modification of
+                # the /etc/gdm3/custom.conf file
                 modifyGDM3CustomConf
             fi
         done
